488647537131ac416aa13ed66451b874336ee9df
Kia Ishii <kia.king.08@gmail.com>
2020-02-12 17:11:03 +0900

    update: v4.0

--- src/mixin.js
+++ src/mixin.js
@@ -1,33 +1,12 @@
-export default function (Vue) {
-  const version = Number(Vue.version.split('.')[0])
+export default function (app, store) {
+  // TODO: Inject store instance to Vue instance to make `this.$store`
+  // available. Not sure how to do that but when the time comes, I
+  // think it would be easy.
 
-  if (version >= 2) {
-    Vue.mixin({ beforeCreate: vuexInit })
-  } else {
-    // override init and inject vuex init procedure
-    // for 1.x backwards compatibility.
-    const _init = Vue.prototype._init
-    Vue.prototype._init = function (options = {}) {
-      options.init = options.init
-        ? [vuexInit].concat(options.init)
-        : vuexInit
-      _init.call(this, options)
-    }
-  }
+  // TODO: Maybe we should use Symbol gor the key...?
+  app.provide('store', store)
 
-  /**
-   * Vuex init hook, injected into each instances init hooks list.
-   */
-
-  function vuexInit () {
-    const options = this.$options
-    // store injection
-    if (options.store) {
-      this.$store = typeof options.store === 'function'
-        ? options.store()
-        : options.store
-    } else if (options.parent && options.parent.$store) {
-      this.$store = options.parent.$store
-    }
-  }
+  app.mixin({
+    inject: ['store']
+  })
 }

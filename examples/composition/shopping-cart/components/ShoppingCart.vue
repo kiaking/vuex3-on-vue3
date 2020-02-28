79eb0590c3e91d2d9e4cf1497c594abd0e5b6674
Kia King Ishii <kia.king.08@gmail.com>
2020-02-28 23:31:54 +0900

    test: add cart e2e test

---
+++ examples/composition/shopping-cart/components/ShoppingCart.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="cart">
+    <h2>Your Cart</h2>
+    <p v-show="!products.length"><i>Please add some products to cart.</i></p>
+    <ul>
+      <li
+        v-for="product in products"
+        :key="product.id">
+        {{ product.title }} - {{ currency(product.price) }} x {{ product.quantity }}
+      </li>
+    </ul>
+    <p>Total: {{ currency(total) }}</p>
+    <p><button :disabled="!products.length" @click="checkout(products)">Checkout</button></p>
+    <p v-show="checkoutStatus">Checkout {{ checkoutStatus }}.</p>
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+import { currency } from '../currency'
+
+export default {
+  setup () {
+    const store = useStore()
+
+    const checkoutStatus = computed(() => store.state.cart.checkoutStatus)
+    const products = computed(() => store.getters['cart/cartProducts'])
+    const total = computed(() => store.getters['cart/cartTotalPrice'])
+
+    const checkout = (products) => store.dispatch('cart/checkout', products)
+
+    return {
+      currency,
+      checkoutStatus,
+      products,
+      total,
+      checkout
+    }
+  }
+  // computed: {
+    // ...mapState({
+    //   checkoutStatus: state => state.cart.checkoutStatus
+    // }),
+  //   ...mapGetters('cart', {
+  //     products: 'cartProducts',
+  //     total: 'cartTotalPrice'
+  //   })
+  // },
+  // methods: {
+  //   checkout (products) {
+  //     this.$store.dispatch('cart/checkout', products)
+  //   }
+  // }
+}
+</script>
